5c6af3a2d699ca8a02e403f24478576184b75ab8
Shraddha Tiwari <118847353+shraddhatiwari25@users.noreply.github.com>
2024-03-21 17:09:17 +0530

    Add files via upload

---
+++ src/app/layout.js
@@ -0,0 +1,19 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Providers } from "@/store/provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Employee Management",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
